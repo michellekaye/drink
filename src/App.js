88c48a1f882da0bf5b98364afdefe5a16655d5f4
Michelle Kaye Lagunsad <mlagunsad@gmail.com>
2023-05-28 10:19:26 -0700

    Test out cocktail endpoints

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const CATEGORIES_URL =
+	"http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+
+const FILTER_BY_CATEGORY_URL =
+	"http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [categories, setCategories] = useState([]);
+
+	const getCategories = async () => {
+		try {
+			const res = await axios.get(CATEGORIES_URL);
+			setCategories(res.data.drinks.map((item) => item.strCategory));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const getCocktails = async (param) => {
+		try {
+			const res = await axios.get(`${FILTER_BY_CATEGORY_URL}${param}`);
+			console.log(res);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getCategories();
+	}, []);
+
+	const handleCategoryClick = (category) => {
+		const param = category.split(" ").join("_");
+		getCocktails(param);
+	};
+
+	return (
+		<main className="App">
+			{categories.map((category) => {
+				return (
+					<button onClick={() => handleCategoryClick(category)}>
+						{category}
+					</button>
+				);
+			})}
+		</main>
+	);
 }
 
 export default App;
